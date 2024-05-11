2a2f6fda08fcf6f1792fe948fad03e8b8a1f1486
yclin <yclin920829@gmail.com>
2024-05-12 06:15:42 +0800

    [Refactoring] Refactored LifeSystem.

--- include/life/LifeSystem.hpp
+++ include/life/LifeSystem.hpp
@@ -4,57 +4,40 @@
 #include "event/EventManager.hpp"
 #include "event/EventType.hpp"
 #include "Util/GameObject.hpp"
-#include "Util/Logger.hpp"
 #include "life/Life.hpp"
-#include "App.hpp"
 
 class LifeSystem : public Util::GameObject {
 public:
-    explicit LifeSystem(EventManager * eventManager) {
-        eventManager->addListener(COLLISION_WITH_GHOST_EVENT_NOT_INVINCIBLE, std::bind(&LifeSystem::onEvent, this, std::placeholders::_1, std::placeholders::_2));
+    explicit LifeSystem(int hitPoints, glm::vec2 position) : hitPoints(hitPoints) {
+        for (int i = 0; i < hitPoints - 1; i++) {
+            std::shared_ptr<Life> life = std::make_shared<Life>();
+            life->SetPosition({position.x, (position.y + (float) i * 32)});
+            this->AddChild(life);
+        }
     };
 
-    void onEvent(EventType eventType, const EventData& eventData){
+    void AddEventManager(EventManager *eventManager) {
+        eventManager->addListener(COLLISION_WITH_GHOST_EVENT_NOT_INVINCIBLE,
+                                  std::bind(&LifeSystem::onEvent, this, std::placeholders::_1, std::placeholders::_2));
+    }
+
+    void onEvent(EventType eventType, const EventData &eventData) {
         if (eventType == COLLISION_WITH_GHOST_EVENT_NOT_INVINCIBLE) {
             LifeDecrease();
         }
     }
 
     void LifeDecrease() {
-        if(lifes.empty()){
-            LOG_DEBUG("the nums of life is already out of cost");
-        }else{
-            this->RemoveChild(this->GetChildren().back());
-            lifes.pop_back();
-            hitPoints--;
-        }
+        this->RemoveChild(this->GetChildren().back());
+        hitPoints--;
     };
 
-    void InitialLifeSystem() {
-        lifes.clear();
-        for (int i = 0; i < hitPoints ; i++){
-            std::shared_ptr<Life> life = std::make_shared<Life>();
-            life->SetPosition({position.x - 32, (position.y + (float)i * 32)});
-            this->AddChild(life);
-            lifes.push_back(life);
-        }
-    };
-
-    void SetSystemPosition(glm::vec2 position) {
-        this->position = position;
-    };
-
-    bool IsDone(){
-        if(hitPoints == 0) return true;
-        return false;
+    [[nodiscard]] bool IsDone() const {
+        return hitPoints == 0;
     }
 
 private:
-    glm::vec2 position {0, 0};
-
-    int hitPoints = 2;
-    std::vector<std::shared_ptr<Life>> lifes;
-
+    int hitPoints;
 };
 
 #endif //REPLACE_WITH_YOUR_PROJECT_NAME_LIFESYSTEM_HPP
